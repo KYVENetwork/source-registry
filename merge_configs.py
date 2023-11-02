923b71b8302208469ca9990a01624d5b5c76e41d
christopherbrumm <christopherbrumm2000@gmail.com>
2023-11-02 12:05:22 +0100

    update: GitHub workflow.

---
+++ merge_configs.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+
+merged_config = {}
+
+# Iterate through all config.yml files
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if file == "config.yml":
+            chain_id = os.path.basename(root)
+            with open(os.path.join(root, file), 'r') as stream:
+                config = yaml.safe_load(stream)
+            merged_config[chain_id] = config
+
+# Write the merged config to a new file
+with open("registry.yml", "w") as output_file:
+    yaml.dump(merged_config, output_file)
